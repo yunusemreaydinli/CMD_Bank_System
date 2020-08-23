51e2a6f5341af59b64c80fb3b265cd7ecd3f84ef
yunusemreaydinli <yunusemreaydinli.4@gmail.com>
2020-08-23 22:56:53 +0300

    version 1.0.2

--- Bank.c
+++ Bank.c
@@ -3,102 +3,114 @@ Yunus Emre Aydýnlý
 22.08.2020 00:11 */
 #include<stdio.h>
 #include<locale.h>
-
 #include<conio.h>   
 
 int language;
 int main() {
 	
-	setlocale(LC_ALL,"turkish");
-	
+	// setlocale(LC_ALL,"turkish");
+
 	int pass, x=10;	
-	
-	printf("--------Ýnserting the card...--------\n");
+
+	printf("\t\t\t\t\t --------------------------------------- \n");
+	printf("\t\t\t\t\t *           Inserting the card...     *\n");
+	printf("\t\t\t\t\t --------------------------------------- \n\n");
 	sleep(2);
-	printf("The card is approved, please wait.");
+	system("CLS");
+	printf("\t\t\t\t\t --------------------------------------- \n");
+	printf("\t\t\t\t\t *           Inserting the card...     *\n");
+	printf("\t\t\t\t\t *  The card is approved, please wait. *\n");
+	printf("\t\t\t\t\t --------------------------------------- \n\n");
 	sleep(2);
 	system("CLS");
 	
-	printf("--------WELCOME TO CMD BANK!--------\n");
- 	printf("Please choose your language! (English = 1 / Turkish = 2 / EXIT: 3): ");
+	printf("\t\t\t\t\t ---------------------------------------- \n");
+	printf("\t\t\t\t\t *           WELCOME TO CMD BANK!       *\n");
+	printf("\t\t\t\t\t ---------------------------------------- \n\n");
+	 	
+ 	printf("\t\t\t\t\t        Please choose your language!    \n");
+	printf("\t\t\t\t\t  (English = 1 / Turkish = 2 / EXIT: 3): ");
  	scanf("%d", &language);
- 	
+
  	if (language == 1) {
- 		printf("Application language is English. Please wait...");
+ 			printf("\n\t\t\t\t\tApplication language is English. Please wait...");
  		
 	 } else if (language == 2) {
-	 		printf("Uygulama dili Türkçe yapýlýyor. Lütfen Bekleyiniz...");
+	 		printf("\n\t\t\t\t\tUygulama dili Turkce yapiliyor. Lutfen Bekleyiniz...");
 	 } 
 	   else if (language == 3) {
 	   	return 0;
 	   }
 	 else {
 	 	system("CLS");
-	 	printf("Error! Please enter the numbers requested from you.");
+	 	//printf("\n\n\n\n\n\n\n\n\n\n\n\n"); //bunu açarsan hata mesajý ortalanýr.
+	 	printf("\t\t\t\t ---------------------------------------------------------\n");
+		printf("\t\t\t\t *  Error! Please enter the numbers requested from you.  *\n");
+		printf("\t\t\t\t ---------------------------------------------------------");
 	 	sleep(1);
 	 	system("CLS");
 	 	
 	return main();
 	 }
  
- 	sleep(1);
+ 		sleep(1);
 	   	system("CLS");
-	
-	if(language == 1) {
-		 	printf("--------CMD BANKASINA HOÞGELDÝNÝZ!--------\n");
-      }
-	else{
-	 	printf("--------WELCOME TO CMD BANK!--------\n");
-	  }
- 	
-	while (x!=0) {
+
+	while (x != 0) {
 		if(language == 1 ) {
 			system("CLS");
-			printf("--------WELCOME TO CMD BANK!--------\n");
-			printf("\nEnter your password: ");
+			printf("\t\t\t\t\t ---------------------------------------- \n");
+			printf("\t\t\t\t\t *           WELCOME TO CMD BANK!       *\n");
+			printf("\t\t\t\t\t ---------------------------------------- \n\n");
+			printf("\t\t\t\t\t\a Enter your password: ");
 
 	   }
 		else {
 			system("CLS");
-			printf("--------CMD BANKASINA HOÞGELDÝNÝZ!--------\n");
-			printf("\nParolanýzý giriniz: ");
+			printf("\t\t\t\t\t ---------------------------------------- \n");
+			printf("\t\t\t\t\t *        CMD BANKASINA HOSGELDINIZ!    *\n");
+			printf("\t\t\t\t\t ---------------------------------------- \n\n");
+			printf("\t\t\t\t\t\a Parolanizi giriniz: ");
 
 		}
-		scanf("%d",&pass);	
-		if (pass == 123)
-		{
+		scanf("%d", &pass);	
+		if (pass == 123) {
 		if(language == 1 ) {
-			printf("Logging in...");
+			printf("\n\t\t\t\t\t Logging in...\n");
 
 		}
 		else {
-			printf("\nGiriþ yapýlýyor...");
+			printf("\n\t\t\t\t\t Giris yapiliyor...\n");
 
 		}
 		x=0;
 		if(language == 1) {
-			printf("\nPlease wait...");
-		}
-		else {
-			printf("\nLütfen Bekleyiniz...");
-		}
-   			sleep(1);
+			printf("\t\t\t\t\t Please wait...");
+			sleep(1);
 	   		system("CLS");
-	    }
+		}
+		
+		else {
+			printf("\t\t\t\t\t Lutfen Bekleyiniz...");
+			sleep(1);
+	   		system("CLS");
+		}	
+	}
 	    else {
 	    if(language == 1) {
-	    	printf("Wrong password, try another password.");   
+	    	printf("\n\t\t\t\t\t Wrong password, try another password."); 
+			sleep(1);
 	    	system("CLS");
 		}
 		else {
-			printf("Yanlýþ þifre, baþka bir þifre deneyin.");
+			printf("\n\t\t\t\t\t Yanlis sifre, baska bir sifre deneyin.");
+			sleep(1);
 			system("CLS"); 
 		}  
 	}
-		printf("\n");
    }
-   
-   system("CLS");
+
+	system("CLS");
 	calistir();
 	
 	return 0;
@@ -110,157 +122,216 @@ void calistir() {
 	int tutar;
 	
 	if(language == 1) {
-		printf("--------WELCOME TO CMD BANK!--------\n");
+		printf("\t\t\t\t\t ----------------------------------------\n");
+		printf("\t\t\t\t\t *           WELCOME TO CMD BANK!       *\n");
+		printf("\t\t\t\t\t ----------------------------------------\n\n");
 	}
 	else {
-		printf("-------- CMD BANKASINA HOÞGELDÝNÝZ! --------\n\n");
-	}
-	
-	if(language == 1) {
-		printf("\t\t\tTransactions\n1. Withdraw money\n2. Deposit\n3. Making Money Transfer\n4. Balance Query\n5. Card Refund\n\n\n");
+		printf("\t\t\t\t\t ----------------------------------------\n");
+		printf("\t\t\t\t\t *        CMD BANKASINA HOSGELDINIZ!    *\n");
+		printf("\t\t\t\t\t ----------------------------------------\n");
+	  //printf("\t\t\t\t\t  |\t\t\t\t\t     |\n");
 
 	}
+
+	if(language == 1) {
+		printf("\n\t\t\t\t\t\t\t  TRANSACTIONS\n\n");				
+		printf("\t\t\t\t\t  -------------------------------------- \n");
+		printf("\t\t\t\t\t | [1] Withdraw money                   |\n");
+		printf("\t\t\t\t\t | [2] Deposit                          |\n");
+		printf("\t\t\t\t\t | [3] Making Money Transfer            |\n");
+		printf("\t\t\t\t\t | [4] Balance Query                    |\n");
+		printf("\t\t\t\t\t | [5] Card Refund                      |\n");
+		printf("\t\t\t\t\t  -------------------------------------- \n\n");
+	}
 	else {
-		printf("\t\t\tÝþlemler\n1. Para Çekme\n2. Para Yatýrma\n3. Havale Yapma\n4. Bakiye Sorgulama\n5. Kart Ýade\n\n\n");
+		printf("\n\t\t\t\t\t\t\t  ISLEMLER\n\n");				
+		printf("\t\t\t\t\t  -------------------------------------- \n");
+		printf("\t\t\t\t\t | [1] Para Cekme                       |\n");
+		printf("\t\t\t\t\t | [2] Para Yatirma                     |\n");	
+		printf("\t\t\t\t\t | [3] Havale Yapma                     |\n");
+		printf("\t\t\t\t\t | [4] Bakiye Sorgulama                 |\n");
+		printf("\t\t\t\t\t | [5] Kart Iade                        |\n");
+		printf("\t\t\t\t\t  -------------------------------------- \n\n");
 	}
 	
 	if(language == 1) {
-		printf("Select the action: ");
+		printf("\t\t\t\t\t  Select the action: ");
 	}
 	else {
-		printf("Ýþlemi seçiniz: ");
+		printf("\t\t\t\t\t  Islem seciniz: ");
 	}
-	scanf("%d",&islem);
+	scanf("%d", &islem);
 	
 	switch(islem) {
 		
-		//TR: Para çekme - EN: Withdraw money
+		//1. TR: Para çekme - EN: Withdraw money
 		case 1 :
 			if(language == 1) {
-				printf("Your balance: %d\n",bakiye);
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t  Your balance           :  %d\n", bakiye);
 			}
 			else {
-				printf("Bakiyeniz: %d\n",bakiye);
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t  Bakiyeniz        :  %d\n", bakiye);
 			}
 			if(language == 1) {
-				printf("Amount to withdraw: ");
+				printf("\t\t\t\t\t  Amount to withdraw     :  ");
 			}
 			else {
-				printf("Çekilecek miktar: ");
+				printf("\t\t\t\t\t  Cekilecek miktar :  ");
 			}
-			scanf("%d",&tutar);
+			scanf("%d", &tutar);
 				if (tutar > bakiye) {
 					if(language == 1) {
-						printf("Insufficient balance!\n");
+						printf("\t\t\t\t\t\a  Insufficient balance!\n");
+						sleep(1);
+						system("CLS");
+						calistir();
 				}
 				else {
-					printf("Yetersiz Bakiye!\n");
+						printf("\n\t\t\t\t\t\a  Yetersiz Bakiye!\n");
+	   					sleep(1);
+						system("CLS");
+						calistir();
 				}
 			}
 			bakiye -= tutar;
 			if(language == 1) {
-				printf("\t\t\tYour remaining balance: %d",bakiye);
+				printf("\t\t\t\t\t  Your remaining balance : %d", bakiye);
+				printf("\n\n\t\t\t\t\t ----------------------------------------\n");
 			}
 			else {
-				printf("\t\t\tKalan Bakiyeniz: %d",bakiye);
+				printf("\t\t\t\t\t  Kalan Bakiyeniz  :  %d", bakiye);
+				printf("\n\n\t\t\t\t\t ----------------------------------------");
 			}
 		break;
 		
-		//TR: Para yatýrma - EN: Deposit money
+		//2. TR: Para yatýrma - EN: Deposit money
 		case 2 :
 			if(language == 1) {
-				printf("Your balance: %d\n",bakiye);
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t  Your balance           : %d\n", bakiye);
 			}
 			else {
-				printf("Bakiyeniz: %d\n",bakiye);
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t  Bakiyeniz         : %d\n", bakiye);
 			}
 			if(language == 1) {
-				printf("Amount to be deposited: ");
+				printf("\t\t\t\t\t  Amount to be deposited : ");
 			}
 			else {
-				printf("Yatýrýlacak tutar: ");
+				printf("\t\t\t\t\t  Yatirilacak tutar : ");
 			}
-			scanf("%d",&tutar);
+			scanf("%d", &tutar);
 			bakiye += tutar;
 			if(language == 1) {
-				printf("Your remaining balance: %d",bakiye);
+				printf("\t\t\t\t\t  Your curret balance    : %d", bakiye);
+		    	printf("\n\n\t\t\t\t\t ----------------------------------------\n");
 			}
 			else {
-				printf("Kalan Bakiyeniz: %d",bakiye);
+				printf("\t\t\t\t\t  Guncel bakiyeniz  : %d", bakiye);
+				printf("\n\n\t\t\t\t\t ----------------------------------------");
 			}
 		break;
 		
 		//TR: Havale yapma - EN: Making a transfer
 		case 3 :
 			if(language == 1) {
-				printf("Your balance: %d\n",bakiye);
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t  Your balance             : %d\n", bakiye);
 			}
 			else {
-				printf("Bakiyeniz: %d\n",bakiye);
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t  Bakiyeniz              : %d\n", bakiye);
 			}
 			if(language == 1) {
-				printf("Amount to be transferred: ");
+				printf("\t\t\t\t\t  Amount to be transferred : ");
 			}
 			else {
-				printf("Havale yapýlacak tutar: ");
+				printf("\t\t\t\t\t  Havale yapilacak tutar : ");
 			}
-			scanf("%d",&tutar);
+			scanf("%d", &tutar);
 				if (tutar > bakiye) {
 					if(language == 1) {
-						printf("Insufficient Balance!\n");
+						printf("\t\t\t\t\t\a  Insufficient balance!\n");
+						sleep(1);
+						system("CLS");
+						calistir();
 					}
 					else {
-						printf("Yetersiz Bakiye!\n");
+						printf("\n\t\t\t\t\t\a  Yetersiz Bakiye!\n");
+						sleep(1);
+						system("CLS");
+						calistir();
 					}
 				}
 			bakiye -= tutar;
 			if(language == 1) {
-				printf("Your remaining balance: %d",bakiye);
+				printf("\t\t\t\t\t  Your remaining balance   : %d", bakiye);
+				printf("\n\n\t\t\t\t\t ----------------------------------------");
 			}
 			else {
-				printf("Kalan Bakiyeniz: %d",bakiye);
+				printf("\t\t\t\t\t  Kalan Bakiyeniz        : %d", bakiye);
+				printf("\n\n\t\t\t\t\t ----------------------------------------");
 			}
 		break;
 			
 		//TR: Bakiye sorgulama - EN: Balance query
 		case 4 :
-		if(language == 1) {
-			printf("Your balance: %d",bakiye);
+			if(language == 1) {
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t  Your balance           :  %d\n", bakiye);
+				printf("\n\t\t\t\t\t ----------------------------------------");
 			}
-		else {
-			printf("Bakiyeniz: %d",bakiye);
+			else {
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t  Bakiyeniz              : %d\n", bakiye);
+				printf("\n\t\t\t\t\t ----------------------------------------");
 			}
 		break;
 		
 		//TR: Kard iade - EN: Card returns
 		case 5 :
-		if(language == 1) {
-			printf("The card is returned.\n");
-		}
-		else {
-			printf("Kart iade edildi.\n");
-		}
+			if(language == 1) {
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t The card has been returned. ");
+				printf("\n\n\t\t\t\t\t ----------------------------------------");
+				return;
+			}
+			else {			
+				printf("\n\t\t\t\t\t ----------------------------------------\n\n");
+				printf("\t\t\t\t\t  Kart iade edildi.");
+				printf("\n\n\t\t\t\t\t ----------------------------------------");
+	    		return;
+			}
 			break;
 			
 		//TR: Yanlýþ sayý girimi - EN: Wrong number entry
 		default :
 			if(language == 1) {
 				system("CLS");
-				printf("Error!\nPlease enter the numbers requested from you.");
+				printf("\n\n\n\n\n\n\n\n\n\n\n");
+				printf("\t\t\t\t ---------------------------------------------------------\n");
+				printf("\t\t\t\t *  Error! Please enter the numbers requested from you.  *\n");
+				printf("\t\t\t\t ---------------------------------------------------------\n");
 			}
 			else {
 				system("CLS");
-				printf("Hata!\nLütfen sizden istenen numaralarý giriniz.");
+				printf("\n\n\n\n\n\n\n\n\n\n\n");
+			 	printf("\t\t\t\t ---------------------------------------------------------\n");
+				printf("\t\t\t\t *   Hata! Lutfen sizden istenen numaralari giriniz.     *\n");
+				printf("\t\t\t\t ---------------------------------------------------------\n");
 			}
 			break;
 	}
 	
 	int durum; 
 		if(language == 1) {
-			printf("\nDo you want to leave the Virtual Bank? YES = 1, TURN BACK = 2 :");
+			printf("\n\n\t\t\t\t\t\a Do you want to leave the Virtual Bank?\n\t\t\t\t\t\tYES = 1, TURN BACK = 2 : ");
 		}
 		else {
-		printf("\nSanal Bankadan ayrýlmak istiyor musunuz? EVET = 1, GERÝ DÖN = 2 :");
+			printf("\n\n\t\t\t\t\t\a Sanal Bankadan ayrilmak istiyor musunuz?\n\t\t\t\t\t\tEVET = 1, GERI DON = 2 : ");
 		}
 	 	scanf("%d", &durum);
 	if(durum == 2){
--- README.md
+++ README.md
@@ -7,14 +7,30 @@ Turkish and English Supported, Banking System with C
 - Bank Password: 123
 
 ## Updates
-[![Version](https://img.shields.io/badge/Version-1.0-green)]()
+[![Version0](https://img.shields.io/badge/Version-1.0.0-green)](.)
 ```diff
-
-
++ CMD Bank System version 1.0.0 is out.
+- Unnecessary lines have been deleted.
+```
+[![Version11](https://img.shields.io/badge/Version-1.0.1-green)](.)
+```diff
++ Card placement notification has been added.
++ Bug fix.
+```
+[![Version21](https://img.shields.io/badge/Version-1.0.2-green)](.)
+```diff
++ The interface has been changed and the interface is centered.
++ Warning sounds have been added.
++ Codes held.
++ Bug fix.
+- Unnecessary lines have been deleted.
+- Turkish characters were deleted. English characters are edited.
 ```
 
 ## Authors
 [Yunus Emre Aydınlı](https://github.com/yunusemreaydinli/)
 
 ## License
+[![Lisence](https://img.shields.io/hexpm/l/apa?label=License)](https://github.com/yunusemreaydinli/CMD_Bank_System/blob/master/LICENSE)
+
 This project is licensed under the Apache License 2.0 - see the [LICENSE](https://github.com/yunusemreaydinli/CMD_Bank_System/blob/master/LICENSE) file for details
